9fd0ad9bf6cfc344859cbb4cbf2029076ef067c8
Hayden Carlson <haydog2@hotmail.com>
2019-09-26 22:55:25 -0700

    Refactor server. Make new thread for receiving all UDP data.

---
+++ src/Server/ReceivePackets.java
@@ -0,0 +1,50 @@
+package Server;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class ReceivePackets extends Thread {
+    private DatagramSocket connection;
+    private Server server;
+
+    public ReceivePackets(DatagramSocket socket, Server server) {
+        this.connection = socket;
+        this.server = server;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                byte[] audioData = new byte[44100];
+                DatagramPacket receive_packet = new DatagramPacket(audioData, audioData.length);
+                connection.receive(receive_packet);
+                System.out.println("Received new packet from: " + receive_packet.getAddress().getHostAddress());
+                ServerClient newServerClient = new ServerClient(receive_packet.getAddress(), receive_packet.getPort());
+                server.addNewClient(newServerClient);
+                this.server.sendToAllClients(audioData, receive_packet.getAddress().getHostAddress(), receive_packet.getPort());
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    private void whileChatting() {
+        while (true) {
+            receiveAudio();
+        }
+    }
+
+    private void receiveAudio() {
+        try {
+            byte[] audioData = new byte[44100];
+            DatagramPacket receive_packet = new DatagramPacket(audioData, audioData.length);
+            connection.receive(receive_packet);
+            System.out.println("Received new packet from: " + receive_packet.getAddress().getHostAddress());
+            ServerClient newServerClient = new ServerClient(receive_packet.getAddress(), receive_packet.getPort());
+            server.addNewClient(newServerClient);
+            this.server.sendToAllClients(audioData, receive_packet.getAddress().getHostAddress(), receive_packet.getPort());
+        } catch (Exception e) {
+
+        }
+    }
+}
