d33b9bca2b046fcb42e8b0d41230a3bc2c53c094
Hayden Carlson <haydog2@hotmail.com>
2019-09-26 22:56:47 -0700

    Remove old code.

--- src/Server/ReceivePackets.java
+++ src/Server/ReceivePackets.java
@@ -27,24 +27,4 @@ public class ReceivePackets extends Thread {
             }
         }
     }
-
-    private void whileChatting() {
-        while (true) {
-            receiveAudio();
-        }
-    }
-
-    private void receiveAudio() {
-        try {
-            byte[] audioData = new byte[44100];
-            DatagramPacket receive_packet = new DatagramPacket(audioData, audioData.length);
-            connection.receive(receive_packet);
-            System.out.println("Received new packet from: " + receive_packet.getAddress().getHostAddress());
-            ServerClient newServerClient = new ServerClient(receive_packet.getAddress(), receive_packet.getPort());
-            server.addNewClient(newServerClient);
-            this.server.sendToAllClients(audioData, receive_packet.getAddress().getHostAddress(), receive_packet.getPort());
-        } catch (Exception e) {
-
-        }
-    }
 }
