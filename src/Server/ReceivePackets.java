3781e8913c970ebc63419d1e771d699a5ac01fa1
Hayden Carlson <haydog2@hotmail.com>
2019-09-28 04:09:46 -0700

    Comment ReceivePackets

--- src/Server/ReceivePackets.java
+++ src/Server/ReceivePackets.java
@@ -15,12 +15,23 @@ public class ReceivePackets extends Thread {
     public void run() {
         while (true) {
             try {
+                // Packet for receiving data
                 byte[] audioData = new byte[44100];
+
+                // Build datagram packet to receive
                 DatagramPacket receive_packet = new DatagramPacket(audioData, audioData.length);
+
+                // Receive data
                 connection.receive(receive_packet);
                 System.out.println("Received new packet from: " + receive_packet.getAddress().getHostAddress());
+
+                // Create new client
                 ServerClient newServerClient = new ServerClient(receive_packet.getAddress(), receive_packet.getPort());
+
+                // Add to array if it doesnt exist
                 server.addNewClient(newServerClient);
+
+                // Emit packets to all connected clients
                 this.server.sendToAllClients(audioData, receive_packet.getAddress().getHostAddress(), receive_packet.getPort());
             } catch (Exception e) {
                 System.out.println(e);
